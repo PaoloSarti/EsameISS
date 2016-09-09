af3780daf3ae94a88acb681f41d7f0613500879d
PaoloSarti <paolo.sarti93@gmail.com>
2016-09-09 11:35:57 +0200

    localhost IPs

--- TestCase2016Analysis/srcMore/it/unibo/ctxDriveRobot/testcase2016analysis.pl
+++ TestCase2016Analysis/srcMore/it/unibo/ctxDriveRobot/testcase2016analysis.pl
@@ -1,9 +1,9 @@
 %====================================================================================
 % Context ctxDriveRobot  SYSTEM-configuration: file it.unibo.ctxDriveRobot.testCase2016Analysis.pl 
 %====================================================================================
-context(ctxdriverobot, "192.168.1.69",  "TCP", "8010" ).  		 
-context(ctxoperator, "192.168.1.2",  "TCP", "8015" ).  		 
-context(ctxasc, "192.168.1.2",  "TCP", "8020" ).  		 
+context(ctxdriverobot, "localhost",  "TCP", "8010" ).  		 
+context(ctxoperator, "localhost",  "TCP", "8015" ).  		 
+context(ctxasc, "localhost",  "TCP", "8020" ).  		 
 %%% -------------------------------------------
 qactor( operator , ctxoperator  ).
 qactor( asc , ctxasc  ).
