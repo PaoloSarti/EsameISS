67e51fd7ed0e4cdc072643089480345d46b0dcb3
Marc_B <marcello@localhost.localdomain>
2016-09-10 12:55:02 +0200

    more tex, test plan

--- TestCase2016Analysis/bin/it/unibo/ctxASCConsole/testcase2016analysis.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% Context ctxASCConsole  SYSTEM-configuration: file it.unibo.ctxASCConsole.testCase2016Analysis.pl 
-%====================================================================================
-context(ctxdriverobot, "localhost",  "TCP", "8010" ).  		 
-context(ctxoperatorinterface, "localhost",  "TCP", "8015" ).  		 
-context(ctxascconsole, "localhost",  "TCP", "8020" ).  		 
-%%% -------------------------------------------
-%%% -------------------------------------------
-
--- TestCase2016Analysis/bin/it/unibo/ctxRemoteRobotControlInterface/testcase2016analysis.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% Context ctxRemoteRobotControlInterface  SYSTEM-configuration: file it.unibo.ctxRemoteRobotControlInterface.testCase2016Analysis.pl 
-%====================================================================================
-context(ctxdriverobot, "localhost",  "TCP", "8010" ).  		 
-context(ctxremoterobotcontrolinterface, "localhost",  "TCP", "8015" ).  		 
-context(ctxascconsole, "localhost",  "TCP", "8020" ).  		 
-%%% -------------------------------------------
-%%% -------------------------------------------
-
