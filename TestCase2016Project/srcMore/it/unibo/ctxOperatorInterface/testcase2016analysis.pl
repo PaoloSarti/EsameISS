315da4e0114aca55bf2fcc2e05a0975725df5eee
PaoloSarti <paolo.sarti93@gmail.com>
2016-09-10 12:11:24 +0200

    Removed the generated files (now ignored) from the remote

--- TestCase2016Project/srcMore/it/unibo/ctxOperatorInterface/testcase2016analysis.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% Context ctxOperatorInterface  SYSTEM-configuration: file it.unibo.ctxOperatorInterface.testCase2016Analysis.pl 
-%====================================================================================
-context(ctxdriverobot, "localhost",  "TCP", "8010" ).  		 
-context(ctxoperatorinterface, "localhost",  "TCP", "8015" ).  		 
-context(ctxascconsole, "localhost",  "TCP", "8020" ).  		 
-%%% -------------------------------------------
-%%% -------------------------------------------
-
