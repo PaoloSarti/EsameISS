e796c53a088e25295ff3ec8b4914ff389262123c
Marc_B <marcello@localhost.localdomain>
2016-09-14 20:36:12 +0200

    added new bot configuration. now ledTheory.pl is probably unusable, need to fix it some changes in .ddr, watch out

--- TestCase2016Project/src/it/unibo/led/ledTheory.pl
+++ TestCase2016Project/src/it/unibo/led/ledTheory.pl
@@ -1,28 +1,30 @@
 
-%createPi4jLed( PinNum)  :-
-% 	actorobj( Actor ),
-%  	Actor <- getOutputEnvView returns OutView ,	
-%	class("it.unibo.devices.qa.DeviceLedPi4jQa") <- createLed( OutView, PinNum ) returns LED.
+createPi4jLed( PinNum)  :-
+ 	actorobj( Actor ),
+  	Actor <- getOutputEnvView returns OutView ,	
+	class("it.unibo.devices.qa.DeviceLedPi4jQa") <- createLed( OutView, PinNum ) returns LED.
 	
 	
-%turnTheLed( on ):-
-%	class("it.unibo.devices.qa.DeviceLedPi4jQa") <- getTheLed  returns LED,
-%	LED <- turnOn .
+turnTheLed( on ):-
+	class("it.unibo.devices.qa.DeviceLedPi4jQa") <- getTheLed  returns LED,
+	LED <- turnOn .
 	
-%turnTheLed( off ):-
-%	class("it.unibo.devices.qa.DeviceLedPi4jQa") <- getTheLed  returns LED,
-%	LED <- turnOff .
+turnTheLed( off ):-
+	class("it.unibo.devices.qa.DeviceLedPi4jQa") <- getTheLed  returns LED,
+	LED <- turnOff .
 
-turnTheLed(on):-
-	class("it.unibo.sartiballanti.utils.Utils") <- executeShellCommand("sudo bash/gpioPinOn.sh 25").
+%turnTheLed(on):-
+%	class("it.unibo.sartiballanti.utils.Utils") <- executeShellCommand("sudo bash/gpioPinOn.sh 25").
 	
-turnTheLed(off):-
-	class("it.unibo.sartiballanti.utils.Utils") <- executeShellCommand("sudo bash/gpioPinOff.sh 25").
+%turnTheLed(off):-
+%	class("it.unibo.sartiballanti.utils.Utils") <- executeShellCommand("sudo bash/gpioPinOff.sh 25").
 /*
 ------------------------------------------------------------
-initialize
+%initialize
 ------------------------------------------------------------
 */
-%initialize  :-   createPi4jLed(25).
-initialize.
+initialize  :-   createPi4jLed(25).
+%initialize.
+initialization(X) :- X.
+
 :- initialization(initialize).
