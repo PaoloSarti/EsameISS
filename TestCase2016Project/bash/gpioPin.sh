8c534681237d4d6db3e64769f57b7c686a2f27a0
Marc_B <marcello@localhost.localdomain>
2016-09-15 11:09:24 +0200

    many fixes, works until back to base

--- TestCase2016Project/bash/gpioPin.sh
+++ TestCase2016Project/bash/gpioPin.sh
@@ -1,6 +1,6 @@
 ﻿
-echo "$1" > /sysc/class/gpio/unexport #
-echo "$1" > /sysc/class/gpio/export #
+echo "$1" > /sys/class/gpio/unexport #
+echo "$1" > /sys/class/gpio/export #
 cd /sys/class/gpio/gpio"$1" #
 
 echo out > direction #
